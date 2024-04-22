ce2de5a227ad8f530f3dee21e22d2185cb46ed4c
omarkarem <omar_karem156@outlook.com>
2024-04-23 00:00:37 +0200

    Inquiry class

---
+++ SE2/src/main/java/project/Inquiry.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import java.util.Date;
+
+public class Inquiry {
+
+    private int inquiryid;
+    private String inquiryinfo;
+    private Date inquirydate;
+
+
+    public Inquiry(int inquiryid, String inquiryinfo, Date inquirydate) {
+        this.inquiryid = inquiryid;
+        this.inquiryinfo = inquiryinfo;
+        this.inquirydate = inquirydate;
+    }
+
+
+    public int getInquiryid() {
+        return inquiryid;
+    }
+
+    public void setInquiryid(int inquiryid) {
+        this.inquiryid = inquiryid;
+    }
+
+    public String getInquiryinfo() {
+        return inquiryinfo;
+    }
+
+    public void setInquiryinfo(String inquiryinfo) {
+        this.inquiryinfo = inquiryinfo;
+    }
+
+    public Date getInquirydate() {
+        return inquirydate;
+    }
+
+    public void setInquirydate(Date inquirydate) {
+        this.inquirydate = inquirydate;
+    }
+
+
+    public void createInquiry() {
+
+        System.out.println("New inquiry created: " + this.inquiryinfo);
+    }
+
+
+    public static void reviewInquiry() {
+
+        System.out.println("Reviewing inquiries");
+ 
+    }
+
+} 
